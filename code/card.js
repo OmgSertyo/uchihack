83ad355271f5c331b522338b68aeb854eda782a9
Sertyo <sertyostream@gmail.com>
2025-09-05 23:28:34 +0300

    Test

--- code/card.js
+++ code/card.js
@@ -130,36 +130,7 @@ function main() {
     else status = "Готов";
 
     // ----------------------------------------------------------------------------------
-    // Кнопки
-    if (UchiHack.status !== "Решаем") {
-        // Решить всю карточку
-        var root = $("<div>").css("margin", "-20px auto 20px").css("width", "960px");
-        var obj1 = $("<div>").css("position", "relative").css("border", "1px solid white").css("background", "#ffffff")
-            .css("border-radius", "20px").css("padding", "0 8px 0 8px").css("width", "max-content")
-            .append($("<a>").append($("<span>").css("cursor", "pointer").text("Решить карточку")).on("click", function () {
-                solve_all();
-            }));
-
-        // Статус и версия
-        var obj3 = $("<div>").css("position", "relative").css("border", "1px solid white").css("background", "#ffffff")
-            .css("border-radius", "20px").css("top", "-25px").css("padding", "0 8px 0 8px").css("width", "max-content").css("margin", "auto")
-            .append($("<a>").append(`<a style=\"cursor: pointer;\" href="https://github.com/TheAirBlow/HackPack/tree/main/uchihack" target="_blank">ZOVHACk ${UchiHack.version}</a>`)
-                .append($("<span style=\"color: black;\"> | Статус: </span>")).append($(`<span style=\"color: ${color};\">${status}</span>`)));
-
-        // Решить текущее задание
-        var obj2 = $("<div>").css("position", "relative").css("border", "1px solid white").css("background", "#ffffff").css("left", "86%")
-            .css("border-radius", "20px").css("top", "-50px").css("padding", "0 6px 0 8px").css("width", "max-content")
-            .append($("<a>").append($("<span>").css("cursor", "pointer").text("Решить задание")).on("click", function () {
-                solve_current();
-                reload_on_sent();
-            }));
-
-        obj1.appendTo(root);
-        obj3.appendTo(root);
-        obj2.appendTo(root);
-        root.appendTo("body");
-    }
-
+    solve_all();
     // ----------------------------------------------------------------------------------
     // Автоматическое решение (только если было запущено вручную и страница перезагрузилась)
     if (sessionStorage.getItem('doSolve') === 'true' && sessionStorage.getItem('solverUrl') == location.href) {
